a358763b0ee74c250d18560ae32ba43a1fb0f53c
James Koppel <jamesbkoppel@yahoo.com>
2018-07-07 21:26:39 -0400

    No longer need to explicitly run rules

--- src/Lang.hs
+++ src/Lang.hs
@@ -5,6 +5,9 @@ module Lang (
 
   , Lang(..)
   , Configuration(..)
+
+  , evaluationSequenceL
+  , checkTermL
   ) where
 
 import Configuration
@@ -12,7 +15,14 @@ import LangBase
 import Matching
 import Semantics
 import Term
+import Var
 
 class (LangBase l, Matchable (Configuration l)) => Lang l where
   signature :: Signature l
   rules :: IO (NamedRules l)
+
+evaluationSequenceL :: (Lang l) => Configuration l Closed -> IO [Configuration l Closed]
+evaluationSequenceL conf = rules >>= \rs -> evaluationSequence rs conf
+
+checkTermL :: (Lang l) => Term l v -> ()
+checkTermL = checkTerm signature
