adf4fab4ec8d0254f50a374c7766687273706a80
Arman Talkar <atalkar@mit.edu>
2019-04-01 00:22:18 -0400

    Broken, but idk man.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,21 @@
 
 module Main where
 
-import Data.List
+import System.Environment
+import System.Exit
+
+import Control.DeepSeq
+import Configuration
+-- import Data.GraphViz
+-- import Data.Graph.Inductive.Example
+-- import Data.GraphViz.Printing
+-- import Data.Graph.Inductive.PatriciaTree
+import Graph
+import Rose
+import Semantics.AbstractMachine
+import Semantics.Conversion
+import Semantics.PAM
+import Term
 
 import Lang
 import Semantics.SOS
@@ -12,7 +26,7 @@ import Languages.Translation
 import Languages.AddMul
 
 import Languages.LockStep
---import Languages.MITScript.Parse
+import Languages.MITScript.Parse as MITParse
 import Languages.MITScript.Semantics
 import Languages.MITScript.Signature
 import Languages.MITScript.Syntax
@@ -20,10 +34,30 @@ import Languages.MITScript.Translate
 
 import Languages.Imp
 
-import Languages.Tiger.Parse
+import Languages.Tiger.Parse as TigerParse
 import Languages.Tiger.Semantics
 import Languages.Tiger.Signature
 import Languages.Tiger.Translate
 
 main :: IO ()
-main = print =<< rules @ImpLang
+main = do
+  args <- getArgs
+  validate args (length args)
+
+usage = "Usage: derive-cfg [language name] [path to source]"
+
+validate :: [String] -> Int -> IO ()
+validate args 2 = makeGraph (head args) (last args)
+validate _ _ = return usage
+
+makeGraph :: String -> String -> IO ()
+makeGraph lang fs = case lang of
+  "mitscript" -> makeCfg (parse fs)
+  _           -> return "Unsupported language"
+
+parse fs = toGeneric (MITParse.parseFile fs) :: Term MITScript
+makeCfg term = do
+  mitrules <- rules term
+  pamRules <- sosToPam mitrules
+  amRules <- pamToAM pamRules
+  abstractAmCfg (irrelevance term) (irrelevance term) amRules term
